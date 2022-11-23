d7af54c6f72f53190b538b3504e8f691e1806bfd
Thien Trieu <thien.trieu@telus.com>
2022-11-23 14:06:15 -0800

    Added client.js and moved connect function over to client.js exported and required on play.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  return conn;
+};
+console.log("Connecting ...");
+connect();
+
+module.exports = {
+  connect,
+};
